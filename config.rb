d12472fba826c94236450ec606c7872720f977ca
marucomerci <marianacomerci@oceano.com.ar>
2019-01-18 20:29:54 -0300

    k

--- config.rb
+++ config.rb
@@ -18,3 +18,12 @@ javascripts_dir = "javascripts"
 # line_comments = false
 
 preferred_syntax = :scss
+
+css_dir = "css" # by Compass.app 
+sass_dir = "sass" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :expanded # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = true # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
